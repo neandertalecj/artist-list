f7a742eadb47282f8d56a3910f7461e48d161bda
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2019-03-03 22:05:27 +0200

    Added some styling with Matirial-ui to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,53 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Icon from '@material-ui/core/Icon'
+import { Home } from '@material-ui/icons'
+import styled from 'styled-components'
+
 
 const Navbar = () => {
   return (
     <AppBar color="primary" position="static">
-      <h1>Artist list</h1>
-      <Link to="/" >
-              <h2>Home</h2>
-      </Link>
-      <Link to="/favorites_page" >
-              <h2>Favorites page</h2>
-      </Link>
+      <NavWrapper>
+        <Toolbar>
+          <Grid container>
+          <Grid item xs={1}>
+              <Link to="/" className="nav-link">
+                {/* <Typography variant="h6" color="inherit"> */}
+                  <Home />
+                {/* </Typography> */}
+              </Link>
+            </Grid>
+            <Grid item xs={5}>
+              <Link to="/" className="nav-link">
+                <Typography variant="h6" color="inherit">
+                  home
+                </Typography>
+              </Link>
+            </Grid>
+            <Grid item xs={6} sm={6}>  
+              <Link to="/favorites_page" className="nav-link">
+                <Typography variant="h6" color="inherit">
+                  favorites
+                </Typography>
+              </Link>
+            </Grid>
+          </Grid>
+         </Toolbar>
+      </NavWrapper>
     </AppBar>
   )
 }
- 
+
+const NavWrapper = styled.nav`
+.nav-link{
+    color: white !important;
+    text-transform: capitalize;
+    text-decoration: none;
+}`
+
 export default Navbar
